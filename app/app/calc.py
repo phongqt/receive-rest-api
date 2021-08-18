1da11ff81b572cdfa0b90d82b0a7dc276cf16248
phong tran <phong.tran@evizi.com>
2021-08-18 17:36:34 +0700

    TDD user model

--- app/app/calc.py
+++
@@ -1,8 +0,0 @@
-def add(x, y):
-    """Add two numbers together"""
-    return x+y
-
-
-def substract(x, y):
-    """Subtract two numbers together"""
-    return x-y
--- app/app/settings.py
+++ app/app/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '''django-insecure-*y*gk*9g&ad
-(s6%-ltcqr3y9w*0tu4d$npzr4ykw=0pgd+g!ip'''
+SECRET_KEY = '''django-insecure-*y*gk*9g&ad(s6%-ltcqr3y9w*0tu4d$npzr4ykw=0pgd+g!ip'''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -87,20 +87,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': '''django.contrib.auth.password_validation.
-        UserAttributeSimilarityValidator''',
+        'NAME': '''django.contrib.auth.password_validation.UserAttributeSimilarityValidator''',
     },
     {
-        'NAME': '''django.contrib.auth.password_validation.
-        MinimumLengthValidator''',
+        'NAME': '''django.contrib.auth.password_validation.MinimumLengthValidator''',
     },
     {
-        'NAME': '''django.contrib.auth.password_validation.
-        CommonPasswordValidator''',
+        'NAME': '''django.contrib.auth.password_validation.CommonPasswordValidator''',
     },
     {
-        'NAME': '''django.contrib.auth.password_validation.
-        NumericPasswordValidator''',
+        'NAME': '''django.contrib.auth.password_validation.NumericPasswordValidator''',
     },
 ]
 
@@ -128,3 +124,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core.User'
